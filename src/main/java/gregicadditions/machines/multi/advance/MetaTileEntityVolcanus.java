b864cab6dadbf761bc7c5c2fbaff420620d8c165
CyanGuy111 <46373872+CyanGuy111@users.noreply.github.com>
2024-03-15 23:59:46 +0700

    Fix volc parallel bug (#14)
    bug

--- src/main/java/gregicadditions/machines/multi/advance/MetaTileEntityVolcanus.java
+++ src/main/java/gregicadditions/machines/multi/advance/MetaTileEntityVolcanus.java
@@ -197,8 +197,9 @@ public class MetaTileEntityVolcanus extends MetaTileEntityElectricBlastFurnace {
             Map<String, Integer> countFluid = new HashMap<>();
             if (matchingRecipe.getFluidInputs().size() != 0) {
 
+
                 this.findFluid(countFluid, fluidInputs);
-                minMultiplier = Math.min(maxItemsLimit, this.getMinRatioItem(countIngredients, matchingRecipe, maxItemsLimit));
+                minMultiplier = Math.min(minMultiplier, this.getMinRatioFluid(countFluid, matchingRecipe, maxItemsLimit));
             }
 
             if (minMultiplier == Integer.MAX_VALUE) {
